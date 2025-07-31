4e0e27792d15824c3ef6a9a8d15830e98c1a200e
Josh Mestemacher <jmestemacher3@gatech.edu>
2025-07-30 20:12:48 -0400

    Added update_database.py, am now able to call LLM successfully and get back a response

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import argparse
 from langchain.prompts import ChatPromptTemplate
-from langchain.vectorstores.chroma import Chroma
-from langchain_community.llms.ollama import Ollama
-from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_ollama import Ollama
+from langchain_ollama import OllamaEmbeddings
 
-from get_embedding_function import get_embedding_function
+from update_database import update_database
 
 
 def main():
-
     # Have user input whether the database should be updated
     while True:
-        
+        question = "Please state whether the database should be updated. Please state either 'Yes' or 'No'.\nYou will get an error if the database is empty and you choose to not populate it and continue on.\n"
 
+        update_input = input(question)
+        # If user inputs "Yes" update the database. If they input "No", continue on and don't update it.
+        if update_input == 'Yes':
+            update_database()
+            break
+        elif update_input == 'No':
+            break
+        # If they don't input "Yes" or "No", reprompt them.
+        else:
+            print("\nPlease state either 'Yes' or 'No'\n")
 
     # Have user give prompt for AI agent
     while True:
@@ -21,16 +30,16 @@ def main():
         if user_prompt:
             break
         else:
-            print("Please give a valid prompt.")
+            print("Please give a valid prompt.\n")
 
     system_prompt = """
-    Answer the given question based just on this:
+    Answer the given question based on this:
 
     {text}
 
     ---
 
-    Answer this question based on the above text: {user_prompt}. If you cannot
+    Answer this question based on the above text: {prompt}. If you cannot
     answer the question, please state as such and explain why. Please answer
     in a structured format.
     """
@@ -42,7 +51,7 @@ def main():
         return embeddings
 
     # Set up the database
-    database = Chroma(persist_directory="chroma", embedding_function=get_embedding_function)
+    database = Chroma(persist_directory="chroma", embedding_function=get_embedding_function())
 
     # Search through the database
     results = database.similarity_search_with_score(user_prompt, k=6)
@@ -50,7 +59,7 @@ def main():
     # Format the results
     context = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(system_prompt)
-    prompt = prompt_template.format(context=context, question="")
+    prompt = prompt_template.format(text=context, prompt=user_prompt)
 
     # Send our prompt to our local LLM
     model = Ollama(model="llama3.1")
@@ -58,7 +67,7 @@ def main():
 
     # Format and show the response to the user
     citations = [doc.metadata.get("id", None) for doc, _score in results]
-    formatted_response = f"Here is the response: {response}\nHere are the Sources: {citations}"
+    formatted_response = f"\nHere is the response:\n{response}\nHere are the Sources: {citations}"
     print(formatted_response)
 
 
---
+++ update_database.py
@@ -0,0 +1,106 @@
+import argparse
+import os
+import shutil
+from langchain_community.document_loaders.pdf import PyPDFDirectoryLoader
+from langchain.schema.document import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_ollama import OllamaEmbeddings
+
+
+
+def update_database():
+    # Have user input whether the database should be resetted before updating
+    while True:
+        reset_input = input("Please state whether the database should be resetted before updating it. Please state either 'Yes' or 'No'.\n")
+        # If user inputs "Yes" reset the database. If they input "No", continue on.
+        if reset_input == 'Yes':
+            reset_database()
+            break
+        elif reset_input == 'No':
+            break
+        # If they don't input "Yes" or "No", reprompt them.
+        else:
+            print("\nPlease state either 'Yes' or 'No'\n")
+
+    # Load the documents in the "data" folder
+    doc_loader = PyPDFDirectoryLoader("data")
+    documents = doc_loader.load()
+
+    # Split the loaded documents into chunks for adding
+    # to the database
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 500,
+        chunk_overlap = 80,
+        length_function=len,
+        is_separator_regex=False
+    )
+    chunks = text_splitter.split_documents(documents)
+
+    # Define the embedding function
+    def get_embedding_function():
+        embeddings = OllamaEmbeddings(model="nomic-embed-text")
+        return embeddings
+
+    # Load the database
+    database = Chroma(
+        persist_directory="chroma", embedding_function=get_embedding_function()
+    )
+
+    # Get page ids
+    chunks_with_ids = get_chunk_ids(chunks)
+
+    # Get existing ids
+    existing_items = database.get(include=[])
+    existing_ids = set(existing_items['ids'])
+
+    # Add new chunks to database
+    new_chunks = []
+    for chunk in chunks_with_ids:
+        if chunk.metadata['id'] not in existing_ids:
+            new_chunks.append(chunk)
+    if len(new_chunks):
+        print(f"Adding new documents to database: {len(new_chunks)}")
+        new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
+        database.add_documents(new_chunks, ids=new_chunk_ids)
+    else:
+        print("There are no new documents to be added\n")
+
+    return
+
+# Function for resetting the database
+def reset_database():
+    # Clear the database if it exists
+    if os.path.exists("chroma"):
+        shutil.rmtree("chroma")
+
+# Function for getting the ids for chunks
+def get_chunk_ids(chunks):
+    # ID's are in the structure of
+    # Page Source: Page Number: Index of Chunk
+    # Example: "Army_FM_Intelligence":3:0
+    
+    previous_page_id = None
+    current_chunk_index = 0
+
+    # Go through each chunk
+    for chunk in chunks:
+        # Get metadata for chunk
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        current_page_id = f"{source}:{page}"
+
+        # Increment chunk index if page ID is same as previous one
+        if current_page_id == previous_page_id:
+            current_chunk_index += 1
+        else:
+            current_chunk_index = 0
+        
+        # Get the chunk id by appending Page ID and chunk index
+        chunk_id = f" {current_page_id}:{current_chunk_index}"
+        previous_page_id = current_page_id
+
+        # Add id to chunk meta-data
+        chunk.metadata["id"] = chunk_id
+    
+    return chunks
